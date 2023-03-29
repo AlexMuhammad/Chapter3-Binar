a627b25d718c8cf5b52745ebadebf8a57d6bfa38
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2023-03-29 22:20:24 +0700

    first commit

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -3,14 +3,21 @@ class App {
     this.clearButton = document.getElementById("clear-btn");
     this.loadButton = document.getElementById("load-btn");
     this.carContainerElement = document.getElementById("cars-container");
+
+    this.inputDate = document.getElementById("inputDate").value;
+    this.inputTime = document.getElementById("inputTime").value;
+    this.inputCapacity = document.getElementById("inputCapacity").value;
+    this.searchBtn = document.getElementById("searchBtn");
+
   }
 
   async init() {
     await this.load();
 
-    // Register click listener
-    this.clearButton.onclick = this.clear;
-    this.loadButton.onclick = this.run;
+    // Register click listener document.addEventListener("click")
+    // this.clearButton.onclick = this.clear;
+    // this.loadButton.onclick = this.run;
+    this.searchBtn.onclick = this.run;
   }
 
   run = () => {
@@ -22,6 +29,7 @@ class App {
   };
 
   async load() {
+    //cars: store list cars yang sudah difilter : [{}]
     const cars = await Binar.listCars();
     Car.init(cars);
   }
